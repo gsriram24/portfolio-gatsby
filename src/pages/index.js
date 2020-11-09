63992b75f71f7e7a118618e73f5cde619e07dc94
G Sriram <sgcuber24@gmail.com>
2020-11-09 12:05:26 +0530

    Added tailwind config and google fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from "react"
-
+import React from 'react';
+import '../css/tailwind.css';
+import './index.css';
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className="text-4xl font-bold">Hello world!</div>;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      'primary-orange': '#FFBB66',
+      'primary-blue': '#005AB4',
+      'primary-dark': '#0B0F13',
+      'secondary-dark': '#1A2026',
+      'accent-dark': '#858585',
+      'primary-light': '#F2F2F2',
+      'secondary-light': '#ffffff',
+      'accent-light': '#3D3D3D',
+    },
+  },
+  variants: {},
+  plugins: [],
+};
